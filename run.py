112f9d49fdf75abc2e969351b86732bfcdd70571
Ismet Cosic <ismet.cosic@gmx.at>
2025-04-07 19:13:28 +0200

    Test

--- run.py
+++ run.py
@@ -2,10 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Eine einfache Route
 @app.route('/')
 def home():
-    return 'Hallo, Welt!'
+    return 'Hallo, Udo!'
 
 if __name__ == '__main__':
     app.run(debug=True)
